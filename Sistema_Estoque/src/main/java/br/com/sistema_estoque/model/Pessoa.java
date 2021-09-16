69f6bfc9d4cb8557829f5612f9f466b69f97800e
MateusFrei <41398101+MateusFrei@users.noreply.github.com>
2021-09-16 17:01:56 -0300

    up

--- Sistema_Estoque/src/main/java/br/com/sistema_estoque/model/Pessoa.java
+++ Sistema_Estoque/src/main/java/br/com/sistema_estoque/model/Pessoa.java
@@ -6,8 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 @Entity
+@Inheritance(Strategy = InheritanceType.JOINED)
 public abstract class Pessoa implements Serializable{
     private static final long serialVersionUID = 1L;
     @Id
