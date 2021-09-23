db2f91668fbae81f3f5e2dad82351e56dd190707
MateusFrei <41398101+MateusFrei@users.noreply.github.com>
2021-09-22 22:56:05 -0300

    up

--- Sistema_Estoque/src/main/java/br/com/sistema_estoque/SistemaEstoqueApplication.java
+++ Sistema_Estoque/src/main/java/br/com/sistema_estoque/SistemaEstoqueApplication.java
@@ -1,4 +1,4 @@
-package we.aplication.sistema_estoque;
+package br.com.sistema_estoque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
